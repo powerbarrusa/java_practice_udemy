7e613de6ca6f9ccc00f47bc0c32d5ced1ebed58c
powerbarrusa <barr@covrigaru.com>
2019-06-19 13:35:10 -0600

    compiled a simple 'hello world' 'app'

--- Variables/src/variables.java
+++ Variables/src/variables.java
@@ -1,5 +1,8 @@
 public class variables {
   public static void main(String[] args) {
-    
+    int age = 31;
+    String name = "Barr";
+    System.out.println("Hello world!");
+    System.out.println(name);
   }
 }
